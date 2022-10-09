394f3f45f6bc25130788b514437c9382cd7539ac
ardentciderman <joe.woodruff@hotmail.com>
2022-10-09 21:23:35 +0100

    Update Block-AzureADUser.ps1

--- M365 Administration/Block-AzureADUser.ps1
+++ M365 Administration/Block-AzureADUser.ps1
@@ -3,4 +3,10 @@
 # We need to set the user associated property BlockCredential 
 # to block user access to Office 365 service
 
+# Block the acount
+
 Set-AzureADUser -ObjectID lisa.pownall@44vpwy.onmicrosoft.com -AccountEnabled $false
+
+# Ensure the account is blocked with a forced sign-out for all apps
+
+Revoke-AzureADUserAllRefreshToken -ObjectId lisa.pownall@44vpwy.onmicrosoft.com
