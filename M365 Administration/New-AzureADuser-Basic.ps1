59a2b31157242fe63a18c5e17cb1efe290b1529c
Joe Woodruff <joe.woodruff@hotmail.com>
2022-10-09 01:20:09 +0100

    Create New-AzureADuser-Basic.ps1

---
+++ M365 Administration/New-AzureADuser-Basic.ps1
@@ -0,0 +1,23 @@
+# This script uses only the minimum parameters needed for creating a user account. 
+# It does not allow the assignment of licenses or add admin roles. We will need to use 
+# separate commands to do that. A key thing to understand before using the New-AzureADUSer
+# cmdlet is that it requires the password to be passed to it using a Password Profile object. 
+# This object contains the password along with other properties such as requiring the user 
+# to change the password upon first login. Understanding that you need a password 
+# profile object informs us that our first steps will be to create the basic password
+# profile in order to provision the new user.
+
+$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$PasswordProfile.Password = “Pas13579"
+$PasswordProfile.ForceChangePasswordNextLogin = $true
+
+# With the password profile created we can now create the base user account:
+
+$params = @{
+    AccountEnable = $true
+    PasswordProfile = $PasswordProfile
+    UserPrincipalName = "rebecca.green@gilbertsblackpool.co.uk"
+    DisplayName = "Rebecca Green"
+}
+
+New-AzureADUser @params
