e048a4b452999e7994250265ef0ba7acda156aab
Joe <66643076+ardentciderman@users.noreply.github.com>
2022-10-30 00:16:28 +0100

    Amend

--- M365 Administration/Set-AzureADUserManager.ps1
+++ M365 Administration/Set-AzureADUserManager.ps1
@@ -1,3 +1,8 @@
-Set-AzureADUserManager
--ObjectId rodney.trotter@44vpwy.onmicrosoft.com 
--RefObjectId 9c8b7ba8-af0a-4ddb-aeb1-6a02347f2cba
+#Set-AzureADUserManager
+#-ObjectId rodney.trotter@44vpwy.onmicrosoft.com 
+#-RefObjectId 9c8b7ba8-af0a-4ddb-aeb1-6a02347f2cba
+
+$User  = "rodney.trotter@44vpwy.onmicrosoft.com"
+$Manager  = "joe.woodruff@44vpwy.onmicrosoft.com"
+$ManagerObj = Get-AzureADUser -ObjectId $Manager
+Set-AzureADUserManager -ObjectId $User -RefObjectId $ManagerObj.ObjectId
