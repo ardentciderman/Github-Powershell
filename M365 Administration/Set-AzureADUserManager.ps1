d32cedfa14a87037beb1160bb6b79e62fd113485
Joe <66643076+ardentciderman@users.noreply.github.com>
2022-10-30 00:58:34 +0100

    Amendment to Script

--- M365 Administration/Set-AzureADUserManager.ps1
+++ M365 Administration/Set-AzureADUserManager.ps1
@@ -1,6 +1,6 @@
-#Set-AzureADUserManager
-#-ObjectId rodney.trotter@44vpwy.onmicrosoft.com 
-#-RefObjectId 9c8b7ba8-af0a-4ddb-aeb1-6a02347f2cba
+# Set the colleague's manager in the AzureAD/M365
+
+Connect-AzureAD -confirm
 
 $User  = "rodney.trotter@44vpwy.onmicrosoft.com"
 $Manager  = "joe.woodruff@44vpwy.onmicrosoft.com"
