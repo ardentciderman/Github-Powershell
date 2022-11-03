c285f8d77561d07afe2514b0c44d68c88b69b6ca
Joe <66643076+ardentciderman@users.noreply.github.com>
2022-11-03 22:00:45 +0000

    3

--- M365 Administration/New-TestMgUser.ps1
+++ M365 Administration/New-TestMgUser.ps1
@@ -2,4 +2,4 @@ $NewPassword = @{}
 $NewPassword["Password"]= "Liverp00l1."
 $NewPassword["ForceChangePasswordNextSignIn"] = $True
 
-New-MgUser -UserPrincipalName "Paul.Nixon@44vpwy.onmicrosoft.com" -DisplayName "Paul Nixon" -PasswordProfile $NewPassword -MailNickName "paul.nixon"
+New-MgUser -UserPrincipalName "Paul.Nixon@44vpwy.onmicrosoft.com" -DisplayName "Paul Nixon" -PasswordProfile $NewPassword -MailNickName "paul.nixon" -AccountEnabled
